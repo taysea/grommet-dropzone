d2fc373f9f14c581eca8aa73eddb2e975acc6273
Taylor Seamans <taylor.seamans@yahoo.com>
2019-12-16 13:06:44 -0800

    remove unnecessary styled component

--- src/components/Dropzone/DropzoneContainer.js
+++ src/components/Dropzone/DropzoneContainer.js
@@ -1,21 +1,10 @@
 import React, { forwardRef } from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Box } from "grommet";
 
-// To avoid nested button errors, this Box is created to allow focus/click behaviors of a button
-// without actually being a button. This container can't be a button because when files are
-// added to the Dropzone, each file contains a 'remove file' button.
-const FocusBox = styled(Box)`
-  cursor: pointer;
-  &:focus {
-    outline: 3px solid #6fffb0;
-  }
-`;
-
 export const DropzoneContainer = forwardRef(
   ({ children, isDragActive, files, ...rest }, ref) => (
-    <FocusBox
+    <Box
       background={isDragActive || files.length ? "white" : "light-2"}
       border={{
         color: isDragActive ? "brand" : "light-5",
@@ -32,7 +21,7 @@ export const DropzoneContainer = forwardRef(
       {...rest}
     >
       {children}
-    </FocusBox>
+    </Box>
   )
 );
 
